02f640a2d73aee76b8733a9450ba1460ecd8cb37
Lee Roy Stevenson <leroysheep@gmail.com>
2025-03-04 12:32:49 +0100

    create Store class

--- store.py
+++ store.py
@@ -20,14 +20,37 @@ class Store:
         return sum([product.get_quantity() for product in self.list_of_products])
 
 
+    def get_all_products(self):
+        '''Returns the list of all active products'''
+        active_products = []
+        for item in self.list_of_products:
+            if item.is_active():
+                active_products.append(item)
+        return active_products
+
+
     def order(self, shopping_list):
         '''Orders a list of products'''
         total_price = 0
-        for product, quantity in shopping_list.items():
+        for product_quantity in shopping_list:
             for p in self.list_of_products:
-                if p.name == product:
-                    total_price += p.buy(quantity)
+                if p == product_quantity[0]:
+                    total_price += p.buy(product_quantity[1])
         return total_price
 
+
+def main():
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    best_buy = Store(product_list)
+    products1 = best_buy.get_all_products()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(products1[0], 1), (products1[1], 2)]))
+
+if __name__ == "__main__":
+    main()
 
 
